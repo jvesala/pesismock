5a97896b4f1f9428cb6b42bd809067c63839cf47
Jussi Vesala <jussi.vesala@reaktor.fi>
2017-03-12 14:44:16 +0200

    add static res dir

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var app = express()
+var port = 5000
+app.set('port', (process.env.PORT || port))
 
-app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/static'))
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.listen(app.get('port'), function () {
-  console.log('Example app listening on port 3000!')
+  console.log('PesisMock started on port ' + port)
 })
